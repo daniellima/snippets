00190cc03b210f40134a43cf988a6d4f39e79a60
Daniel Lima dos Santos <daniel.santos@stone.com.br>
2018-09-28 19:02:37 +0000

    Play with dotnet

---
+++ dotnet-getting-started/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace dotnet_getting_started
+{
+    public class Account {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Account account = new Account {
+                Name = "John Doe",
+                Email = "john@example.com",
+                DOB = new DateTime(1990, 2, 20, 0, 0, 0, DateTimeKind.Utc),
+            };
+
+            string json = JsonConvert.SerializeObject(account, Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
