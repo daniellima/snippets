065c54283c4208f85cfeb89fac25c374714efcb0
Daniel Lima <daniellima.pessoal@gmail.com>
2022-10-16 19:28:18 -0300

    poc of access to mongodb

--- netlify/netlify/functions/hello/index.js
+++ netlify/netlify/functions/hello/index.js
@@ -1,6 +1,16 @@
+const { MongoClient } = require('mongodb');
+
 exports.handler = async function (event, context) {
+    const client = new MongoClient('mongodb+srv://app1:iamQsffYgOlnTKae@data-1.othux.mongodb.net/?retryWrites=true&w=majority');
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db('all');
+    const collection = db.collection('test');
+
+    let n = collection.countDocuments()
+
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Return from function!" }),
+        body: JSON.stringify({ message: "Return from function!", "count": n }),
     };
 };
