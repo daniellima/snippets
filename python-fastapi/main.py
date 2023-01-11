2b065c518b90b705390edd3bafc5e8df4918be43
Daniel Lima <daniellima.pessoal@gmail.com>
2023-01-11 11:28:38 -0300

    add python-fastip playground

---
+++ python-fastapi/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Query, Depends
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"Hello World!"}
+
+@app.get("/repeat/{this}/{times}/times")
+def repeat_many_times(this: str, times: int):
+    return [this for _ in range(times)]
+
+@app.get("/repeat/{this}")
+def repeat_many_times_with_query(this: str, times: int = Query(le=10)):
+    return [this for _ in range(times)]
+
+def get_previous_solution() -> bool:
+    return True
+
+def get_solution(previous_solution: bool = Depends(get_previous_solution)) -> bool:
+    return not previous_solution
+
+@app.get("/itsgood")
+def itsgood(decision : bool = Depends(get_solution)) -> str:
+    return f"the answer is: {decision}"
